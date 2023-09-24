96162666f3ec55ac6d32d4f06ceeb66f7852d5a5
meizhennan <meizhennan@zuoyebang.com>
2023-09-24 17:15:48 +0800

    test

--- src/main/java/com/tencent/wxcloudrun/dto/MessageRequest.java
+++ src/main/java/com/tencent/wxcloudrun/dto/MessageRequest.java
@@ -1,14 +1,21 @@
 package com.tencent.wxcloudrun.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class MessageRequest {
+    @JsonProperty("ToUserName")
     private String ToUserName;
+    @JsonProperty("FromUserName")
     private String FromUserName;
+    @JsonProperty("CreateTime")
     private long CreateTime;
+    @JsonProperty("MsgType")
     private String MsgType;
+    @JsonProperty("Content")
     private String Content;
+    @JsonProperty("MsgId")
     private long MsgId;
 
 }
